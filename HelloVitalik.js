73cf89bf3cf4b740e86f35de338b7fcada41de21
lbjyang <yangfan6262008@gmail.com>
2023-09-01 10:58:20 +0800

    delete ethers

--- HelloVitalik.js
+++ HelloVitalik.js
@@ -1,7 +1,7 @@
 import {ethers} from "ethers";
-const provider = ethers.getDefaultProvider();
+const provider = ethers.getDefaultProvider()
 const main = async() => {
-    const balance = await provider.getBalance('Vitalik.eth');
-    console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);
+    const balance = await provider.getBalance('vitalik.eth');
+    console.log(`ETH Balance of vitalik: ${ethers.utils.formatEther(balance)} ETH`);
 }
 main()
